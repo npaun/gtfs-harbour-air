b232328d7ea7f3efe39ad8f928faa60b4cb8e9f8
Nicholas Paun <nicholas@transit.app>
2024-06-18 08:20:55 -0700

    Super close to a GTFS export now

--- convert/timetables.py
+++ convert/timetables.py
@@ -1,6 +1,7 @@
 import camelot
 import sys
 from dataclasses import dataclass, field
+import dataclasses
 import datetime
 import csv
 from pathlib import Path
@@ -20,7 +21,6 @@ class MismatchError(Exception):
 class Placemarks:
     origin: str = ''
     dest: str = ''
-    direction_id: int = -1 
 
     @classmethod
     def parse(cls, text, delim):
@@ -31,23 +31,11 @@ class Placemarks:
     def canonicalize(text):
         return RE_SPACE.sub(' ', text.replace('-', ' '))
 
-    def set_direction_wrt(self, other):
-        if (other.origin, other.dest) == (self.origin, self.dest):
-            self.direction_id = 0
-        elif (other.origin, other.dest) == (self.dest, self.origin):
-            self.direction_id = 1
-        else:
-            raise MismatchError()
-
 
 @dataclass
-class Flight:
-    placemarks: Placemarks
-    flight_no: int
+class Calendar:
     start_date: datetime.date
     end_date: datetime.date
-    departure_time: datetime.time
-    arrival_time: datetime.time
     monday: bool
     tuesday: bool
     wednesday: bool
@@ -56,11 +44,90 @@ class Flight:
     saturday: bool
     sunday: bool
 
+    @staticmethod
+    def gtfs_date(date_val):
+        return date_val.strftime('%Y%m%d')
+
+    @property
+    def day_mask(self):
+        days = [self.monday, self.tuesday, self.wednesday, self.thursday, self.friday, self.saturday, self.sunday]
+        return ''.join(str(int(day)) for day in days)
+
+    @property
+    def service_id(self):
+        return f'{Calendar.gtfs_date(self.start_date)}-{Calendar.gtfs_date(self.end_date)}-{self.day_mask}'
+
+    def serialize(self):
+        return (self.service_id, 
+                Calendar.gtfs_date(self.start_date), 
+                Calendar.gtfs_date(self.end_date),
+                self.monday,
+                self.tuesday,
+                self.wednesday,
+                self.thursday,
+                self.friday,
+                self.saturday,
+                self.sunday
+                )
+
+
+@dataclass
+class StopTime:
+    stop_id: str
+    arrival_time: datetime.time
+    departure_time: datetime.time
+    trip_id: str = ''
+    stop_sequence: int = -1
+
+    @staticmethod
+    def gtfs_time(time_val):
+        return time_val.strftime('%H:%M:%S')
+
+    def serialize(self):
+        return (self.trip_id,
+                self.stop_sequence,
+                self.stop_id,
+                StopTime.gtfs_time(self.arrival_time),
+                StopTime.gtfs_time(self.departure_time)
+                )
+
+
+@dataclass
+class Trip:
+    route_id: str
+    trip_short_name: int
+    service: Calendar
+    direction_id: int
+    trip_headsign: str
+    stop_times: list[StopTime] = field(default_factory=list)
+
+    @property
+    def trip_id(self):
+        return f'YB{self.trip_short_name}-{self.service.service_id}'
+
+    def add_stop_time(self, st):
+        st.trip_id = self.trip_id
+        st.stop_sequence = len(self.stop_times)
+        self.stop_times.append(st)
+
+    def serialize(self):
+        return (self.trip_id,
+                self.route_id,
+                self.service.service_id,
+                self.direction_id,
+                self.trip_headsign,
+                self.trip_short_name
+                )
+
+
 @dataclass
 class ParseState:
+    route: Any = None
     expected_placemarks: Placemarks = Placemarks()
-    flights: list[Flight] = field(default_factory=list)
+    stops: list[str] = field(default_factory=list)
+    flights: list[Trip] = field(default_factory=list)
     placemarks: Placemarks = Placemarks()
+    direction_id: int = -1
     last_row: list[Any] = field(default_factory=list) 
 
     def next_file(self):
@@ -69,8 +136,22 @@ class ParseState:
     def next_table(self):
         self.last_row = []
 
+    @staticmethod
+    def get_direction_wrt(expected, actual):
+        expected_tpl = dataclasses.astuple(expected)
+        actual_tpl = dataclasses.astuple(actual)
+        if expected_tpl == actual_tpl:
+            return 0
+        elif expected_tpl == tuple(reversed(actual_tpl)):
+            return 1
+        else:
+            raise MismatchError()
+
+
 def parse_route(schedule_dir, route, state):
+    state.route = route
     state.expected_placemarks = Placemarks.parse(route.route_long_name, ' - ')
+    state.stops = route.route_id.split('-')
     candidate_scheds = list(schedule_dir.glob(f'{route.npaun_series_id}[-_ ]*.pdf'))
 
     for pdf_path in candidate_scheds:
@@ -95,28 +176,61 @@ def parse_table(tbl, state):
         nn_row = nonnull(row)
         if is_header(nn_row):
             state.placemarks = Placemarks.parse(state.last_row[0], ' to ')
-            state.placemarks.set_direction_wrt(state.expected_placemarks)
+            state.direction_id = ParseState.get_direction_wrt(state.expected_placemarks, state.placemarks)
         elif len(nn_row) == 1:
             pass
         else:
             rs_row = resplit(nn_row)
-            state.flights.append(Flight(
-                    placemarks=state.placemarks,
-                    flight_no=int(rs_row[0]),
-                    start_date=datetime.datetime.strptime(rs_row[1], '%d-%b-%y'),
-                    end_date=datetime.datetime.strptime(rs_row[2], '%d-%b-%y'),
-                    departure_time=datetime.datetime.strptime(f'{rs_row[3]} {rs_row[4]}', '%I:%M %p'),
-                    arrival_time=datetime.datetime.strptime(f'{rs_row[5]} {rs_row[6]}', '%I:%M %p'),
-                    monday=rs_row[7] == 'M',
-                    tuesday=rs_row[8] == 'Tu',
-                    wednesday=rs_row[9] == 'W',
-                    thursday=rs_row[10] == 'Th',
-                    friday=rs_row[11] == 'F',
-                    saturday=rs_row[12] == 'Sa',
-                    sunday=rs_row[13] == 'Su'
-                ))
+            trip = Trip(
+                    route_id=state.route.route_id,
+                    trip_short_name=int(rs_row[0]),
+                    direction_id=state.direction_id,
+                    trip_headsign=state.placemarks.dest,
+                    service=Calendar(
+                        start_date=parse_date(rs_row[1]),
+                        end_date=parse_date(rs_row[2]),
+                        monday=rs_row[7] == 'M',
+                        tuesday=rs_row[8] == 'Tu',
+                        wednesday=rs_row[9] == 'W',
+                        thursday=rs_row[10] == 'Th',
+                        friday=rs_row[11] == 'F',
+                        saturday=rs_row[12] == 'Sa',
+                        sunday=rs_row[13] == 'Su'
+                    )
+            )
+            
+            departure_time = parse_time(rs_row[3], rs_row[4])
+            arrival_time = parse_time(rs_row[5], rs_row[6])
+            stops = state.stops if state.direction_id == 0 else list(reversed(state.stops))
+
+            trip.add_stop_time(StopTime(
+                stop_id=stops[0],
+                arrival_time=departure_time,
+                departure_time=departure_time,
+            ))
+
+            trip.add_stop_time(StopTime(
+                stop_id=stops[1],
+                arrival_time=arrival_time,
+                departure_time=arrival_time
+            ))
+
+            print(trip.serialize())
+            print(trip.service.serialize())
+            print(trip.stop_times[0].serialize())
+            print(trip.stop_times[1].serialize())
+
+            state.flights.append(trip)
+
         state.last_row = nn_row
 
+
+def parse_date(value):
+    return datetime.datetime.strptime(value, '%d-%b-%y').date() 
+
+
+def parse_time(time_val, ampm_val):
+    return datetime.datetime.strptime(f'{time_val} {ampm_val}', '%I:%M %p').time()
 
 def is_header(row):
     return row[0] == 'Flight #'
